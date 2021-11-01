1f5f5487b6a5560638b524417c89b061d1a5b90c
Jonas Mosmann <jonasmosmann@web.de>
2021-11-01 13:07:35 +0100

    Task template created

---
+++ templates/Tasks/index.php
@@ -0,0 +1,16 @@
+<table>
+<?php foreach ($tasks as $task){ ?>
+            <tr>
+            <td>
+                    <?= $this->Html->link($task->Title, ['action' => 'view', $task->slug]) ?>
+                </td>
+                <td><?= $task->Description ?></td>
+                <td><?= $task->Completed ?></td>
+                <td><?= $task->Deleted ?></td>
+                <td><?= $task->EntryTime ?></td>
+                <td><?=  $task->Owner ?></td>
+                
+            </tr>
+<?php } ?>
+
+</table>
