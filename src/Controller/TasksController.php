d3b2b0095215368ac5da9e33110338107c13b777
Jonas Mosmann <jonasmosmann@web.de>
2021-11-01 13:35:50 +0100

    Tasks can be added

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -4,12 +4,35 @@ namespace App\Controller;
 
 class TasksController extends AppController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+    }
+
+
+
     public function index()
     {
-        
-        $this->loadComponent('Paginator');
         $tasks = $this->Paginator->paginate($this->Tasks->find());
 
         $this->set('tasks', $tasks);
     }
+    public function add(){
+        $task = $this->Tasks->newEmptyEntity();
+
+        if ($this->request->is('post')){
+            $task = $this->Tasks->patchEntity($task, $this->request->getData());
+        
+            if($this->Tasks->save($task)){
+                $this->Flash->success("Deine Aufgabe wurde notiert!");
+                return $this->redirect(['action'=>'index']);
+            }
+            $this->Flash->error("Deine Aufgabe konnte nicht gespeichert werden!");
+
+        }
+        $this->set('task', $task);
+    }
+
+
 }
--- templates/Tasks/index.php
+++ templates/Tasks/index.php
@@ -7,7 +7,7 @@
                 <td><?= $task->Description ?></td>
                 <td><?= $task->Completed ?></td>
                 <td><?= $task->Deleted ?></td>
-                <td><?= $task->EntryTime ?></td>
+                <td><?= $task->EntryTime->format(DATE_RFC850) ?></td>
                 <td><?=  $task->Owner ?></td>
                 
             </tr>
